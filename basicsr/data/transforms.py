634881c3de71da97e1a7f68f1495f6d7677e07bf
Khoa D. Vo <khoa.synthesis@gmail.com>
2022-09-29 16:52:51 +0700

    add more transforms

--- basicsr/data/transforms.py
+++ basicsr/data/transforms.py
@@ -1,6 +1,8 @@
 import cv2
 import random
 import torch
+import torchvision.transforms.functional as TF
+import numpy as np
 
 
 def mod_crop(img, scale):
@@ -89,6 +91,227 @@ def paired_random_crop(img_gts, img_lqs, gt_patch_size, scale, gt_path=None):
     if len(img_lqs) == 1:
         img_lqs = img_lqs[0]
     return img_gts, img_lqs
+
+
+def paired_random_crop_return_indexes(img_gts, img_lqs, gt_patch_size, scale, gt_path=None):
+    """Paired random crop. Support Numpy array and Tensor inputs.
+
+    It crops lists of lq and gt images with corresponding locations.
+
+    Args:
+        img_gts (list[ndarray] | ndarray | list[Tensor] | Tensor): GT images. Note that all images
+            should have the same shape. If the input is an ndarray, it will
+            be transformed to a list containing itself.
+        img_lqs (list[ndarray] | ndarray): LQ images. Note that all images
+            should have the same shape. If the input is an ndarray, it will
+            be transformed to a list containing itself.
+        gt_patch_size (int): GT patch size.
+        scale (int): Scale factor.
+        gt_path (str): Path to ground-truth. Default: None.
+
+    Returns:
+        list[ndarray] | ndarray: GT images and LQ images. If returned results
+            only have one element, just return ndarray.
+    """
+
+    if not isinstance(img_gts, list):
+        img_gts = [img_gts]
+    if not isinstance(img_lqs, list):
+        img_lqs = [img_lqs]
+
+    # determine input type: Numpy array or Tensor
+    input_type = 'Tensor' if torch.is_tensor(img_gts[0]) else 'Numpy'
+
+    if input_type == 'Tensor':
+        h_lq, w_lq = img_lqs[0].size()[-2:]
+        h_gt, w_gt = img_gts[0].size()[-2:]
+    else:
+        h_lq, w_lq = img_lqs[0].shape[0:2]
+        h_gt, w_gt = img_gts[0].shape[0:2]
+    lq_patch_size = gt_patch_size // scale
+
+    if h_gt != h_lq * scale or w_gt != w_lq * scale:
+        raise ValueError(f'Scale mismatches. GT ({h_gt}, {w_gt}) is not {scale}x ',
+                         f'multiplication of LQ ({h_lq}, {w_lq}).')
+    if h_lq < lq_patch_size or w_lq < lq_patch_size:
+        raise ValueError(f'LQ ({h_lq}, {w_lq}) is smaller than patch size '
+                         f'({lq_patch_size}, {lq_patch_size}). '
+                         f'Please remove {gt_path}.')
+
+    # randomly choose top and left coordinates for lq patch
+    top = random.randint(0, h_lq - lq_patch_size)
+    left = random.randint(0, w_lq - lq_patch_size)
+
+    # crop lq patch
+    if input_type == 'Tensor':
+        img_lqs = [v[:, :, top:top + lq_patch_size, left:left + lq_patch_size] for v in img_lqs]
+    else:
+        img_lqs = [v[top:top + lq_patch_size, left:left + lq_patch_size, ...] for v in img_lqs]
+
+    # crop corresponding gt patch
+    top_gt, left_gt = int(top * scale), int(left * scale)
+    if input_type == 'Tensor':
+        img_gts = [v[:, :, top_gt:top_gt + gt_patch_size, left_gt:left_gt + gt_patch_size] for v in img_gts]
+    else:
+        img_gts = [v[top_gt:top_gt + gt_patch_size, left_gt:left_gt + gt_patch_size, ...] for v in img_gts]
+    if len(img_gts) == 1:
+        img_gts = img_gts[0]
+    if len(img_lqs) == 1:
+        img_lqs = img_lqs[0]
+    return img_gts, img_lqs, top, left
+
+
+def paired_random_crop_by_indexes(img_gts, img_lqs, gt_patch_size, scale, top, left, gt_path=None):
+    """Paired random crop. Support Numpy array and Tensor inputs.
+
+    It crops lists of lq and gt images with corresponding locations.
+
+    Args:
+        img_gts (list[ndarray] | ndarray | list[Tensor] | Tensor): GT images. Note that all images
+            should have the same shape. If the input is an ndarray, it will
+            be transformed to a list containing itself.
+        img_lqs (list[ndarray] | ndarray): LQ images. Note that all images
+            should have the same shape. If the input is an ndarray, it will
+            be transformed to a list containing itself.
+        gt_patch_size (int): GT patch size.
+        scale (int): Scale factor.
+        gt_path (str): Path to ground-truth. Default: None.
+
+    Returns:
+        list[ndarray] | ndarray: GT images and LQ images. If returned results
+            only have one element, just return ndarray.
+    """
+
+    if not isinstance(img_gts, list):
+        img_gts = [img_gts]
+    if not isinstance(img_lqs, list):
+        img_lqs = [img_lqs]
+
+    # determine input type: Numpy array or Tensor
+    input_type = 'Tensor' if torch.is_tensor(img_gts[0]) else 'Numpy'
+
+    if input_type == 'Tensor':
+        h_lq, w_lq = img_lqs[0].size()[-2:]
+        h_gt, w_gt = img_gts[0].size()[-2:]
+    else:
+        h_lq, w_lq = img_lqs[0].shape[0:2]
+        h_gt, w_gt = img_gts[0].shape[0:2]
+    lq_patch_size = gt_patch_size // scale
+
+    if h_gt != h_lq * scale or w_gt != w_lq * scale:
+        raise ValueError(f'Scale mismatches. GT ({h_gt}, {w_gt}) is not {scale}x ',
+                         f'multiplication of LQ ({h_lq}, {w_lq}).')
+    if h_lq < lq_patch_size or w_lq < lq_patch_size:
+        raise ValueError(f'LQ ({h_lq}, {w_lq}) is smaller than patch size '
+                         f'({lq_patch_size}, {lq_patch_size}). '
+                         f'Please remove {gt_path}.')
+
+    # # randomly choose top and left coordinates for lq patch
+    # top = random.randint(0, h_lq - lq_patch_size)
+    # left = random.randint(0, w_lq - lq_patch_size)
+
+    # crop lq patch
+    if input_type == 'Tensor':
+        img_lqs = [v[:, :, top:top + lq_patch_size, left:left + lq_patch_size] for v in img_lqs]
+    else:
+        img_lqs = [v[top:top + lq_patch_size, left:left + lq_patch_size, ...] for v in img_lqs]
+
+    # crop corresponding gt patch
+    top_gt, left_gt = int(top * scale), int(left * scale)
+    if input_type == 'Tensor':
+        img_gts = [v[:, :, top_gt:top_gt + gt_patch_size, left_gt:left_gt + gt_patch_size] for v in img_gts]
+    else:
+        img_gts = [v[top_gt:top_gt + gt_patch_size, left_gt:left_gt + gt_patch_size, ...] for v in img_gts]
+    if len(img_gts) == 1:
+        img_gts = img_gts[0]
+    if len(img_lqs) == 1:
+        img_lqs = img_lqs[0]
+    return img_gts, img_lqs
+
+
+def unpaired_random_crop(img_lqs, img_refs, if_fix, patch_size):
+
+    if not isinstance(img_lqs, list):
+        img_lqs = [img_lqs]
+    if not isinstance(img_refs, list):
+        img_refs = [img_refs]
+
+    h_lq, w_lq, _ = img_lqs[0].shape
+    h_ref, w_ref, _ = img_refs[0].shape
+
+    if h_lq < patch_size or w_lq < patch_size:
+        raise ValueError(f'LQ ({h_lq}, {w_lq}) is smaller than patch size '
+                         f'({patch_size}, {patch_size}). ')
+
+    # randomly choose top and left coordinates for lq patch
+    if if_fix:
+        top = random.randint(0, h_lq - patch_size)
+        left = random.randint(0, w_lq - patch_size)
+        img_lqs = [v[top:top + patch_size, left:left + patch_size, ...] for v in img_lqs]
+        top = random.randint(0, h_ref - patch_size)
+        left = random.randint(0, w_ref - patch_size)
+        img_refs = [v[top:top + patch_size, left:left + patch_size, ...] for v in img_refs]
+    else:
+        ratio_h = np.random.uniform(0.6, 1.0)
+        ratio_w = np.random.uniform(0.6, 1.0)
+        size_h = round(min(h_lq, h_ref) * ratio_h)
+        size_w = round(min(w_lq, w_ref) * ratio_w)
+        top = random.randint(0, h_lq - size_h)
+        left = random.randint(0, w_lq - size_w)
+        img_lqs = [v[top:top + size_h, left:left + size_w, ...] for v in img_lqs]
+        top = random.randint(0, h_ref - size_h)
+        left = random.randint(0, w_ref - size_w)
+        img_refs = [v[top:top + size_h, left:left + size_w, ...] for v in img_refs]
+
+    if len(img_lqs) == 1:
+        img_lqs = img_lqs[0]
+    if len(img_refs) == 1:
+        img_refs = img_refs[0]
+    return img_lqs, img_refs
+
+
+def random_crop(imgs, if_fix, patch_size):
+
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+
+    print(imgs[0].shape)
+
+    h_lq, w_lq, _ = imgs[0].shape
+
+    if h_lq < patch_size or w_lq < patch_size:
+        raise ValueError(f'LQ ({h_lq}, {w_lq}) is smaller than patch size '
+                         f'({patch_size}, {patch_size}). ')
+
+    # randomly choose top and left coordinates for lq patch
+    if if_fix:
+        top = random.randint(0, h_lq - patch_size)
+        left = random.randint(0, w_lq - patch_size)
+        imgs = [v[top:top + patch_size, left:left + patch_size, ...] for v in imgs]
+    else:
+        ratio_h = np.random.uniform(0.6, 1.0)
+        ratio_w = np.random.uniform(0.6, 1.0)
+        size_h = round(h_lq * ratio_h)
+        size_w = round(w_lq * ratio_w)
+        top = random.randint(0, h_lq - size_h)
+        left = random.randint(0, w_lq - size_w)
+        imgs = [v[top:top + size_h, left:left + size_w, ...] for v in imgs]
+
+    if len(imgs) == 1:
+        imgs = imgs[0]
+
+    return imgs
+
+
+def distort_visual_style(img):
+    img = TF.to_pil_image(img)
+    img = TF.adjust_brightness(img, random.uniform(0.7, 1.3))
+    img = TF.adjust_contrast(img, random.uniform(0.7, 1.3))
+    img = TF.adjust_hue(img, random.uniform(-0.1, 0.1))
+    img = TF.adjust_saturation(img, random.uniform(0.9, 1.1))
+    img = TF.to_tensor(img)
+
+    return img
 
 
 def augment(imgs, hflip=True, rotation=True, flows=None, return_status=False):
